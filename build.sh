d03e6ff205cac1e7c843f9ebcaeb528cf8c2e7c4
Ubuntu <carlosginescarretero@gmail.com>
2022-06-21 19:13:05 +0000

    New flags, some test  builds commented out

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ fi
 #gcc -g src/main.c -o build/debug/main.exe
 # ggdb debugging info for gdb debugger
 # -std=gnull enable C99 inline semantics
-gcc -std=gnu11 -ggdb src/main.c -o build/debug/main.exe
+gcc -Wall -DDAEMON=1 -DDEBUG=0 -DDISABLE_STDIO=1 -std=gnu11 -ggdb src/main.c -o build/debug/main.exe
+#gcc -Wall -std=gnu11 -ggdb src/debug_printf.c -o build/debug/debug_printf.exe
 
 #gcc -g src/test.c -o build/debug/test.exe
